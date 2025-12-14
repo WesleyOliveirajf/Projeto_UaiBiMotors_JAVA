7f7c3c3d8f1dbb0bab61cd20206f96e7ab1a9f42
Wesley Oliveira <lojastioinfo@gmail.com>
2025-12-13 22:04:29 -0300

    Adiciona perfil de desenvolvimento Java para Cursor/VSCode

--- src/Carro.java
+++ src/Carro.java
@@ -4,23 +4,89 @@ public class Carro {
     Carro c1, c2;
 
     // Caracteristicas
-    String marca;
-    String modelo;
-    int ano;
-    String cor;
-    int km;
-    double preco;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String cor;
+    private int km;
+    private double preco;
+
+
+    public Carro getC1() {
+        return c1;
+    }
+
+    public void setC1(Carro c1) {
+        this.c1 = c1;
+    }
+
+    public Carro getC2() {
+        return c2;
+    }
+
+    public void setC2(Carro c2) {
+        this.c2 = c2;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
 
     // Criar um "BOTAO" para que eu possa execultar algo (Mostrar os dados)
 
-    void exibirAnuncio(){
+    public void exibirAnuncio(){
         System.out.println("Anuncio do Carro");
         System.out.println(marca+"-"+modelo+" ("+ano+")" );
         System.out.println("cor: "+cor+ " KM: "+ km+" R$ "+preco);
     }
 
     // SIMULA SEGURO
-    void simuladorSeguro(int perfil){
+    public void simuladorSeguro(int perfil){
         if (perfil == 0) {
             System.out.println("Perfil: Idoso - 1.5%");
             System.out.println("R$" + preco*0.015);
@@ -37,7 +103,7 @@ public class Carro {
     }
 
     // CALCULA IPVA
-    double calcularIpva() {
+    public double calcularIpva() {
         double valorIPVA;
         if (ano <= 2022) {
             valorIPVA = 0;
--- src/UaiBiMotors.java
+++ src/UaiBiMotors.java
@@ -6,22 +6,22 @@ public class UaiBiMotors {
         c1 = new Carro();
         c2 = new Carro ();
 
-        c1.marca = " Porsche";
-        c1.modelo = "Cayenne";
-        c1.ano = 2024;
-        c1.km = 0;
-        c1.cor = "Branca";
-        c1.preco = 2200000.0;
+        c1.setMarca(" Porsche");
+        c1.setModelo("Cayenne");
+        c1.setAno(2024);
+        c1.setKm(0);
+        c1.setCor("Branca");
+        c1.setPreco(2200000.0);
 
-        c2.marca = "Chevrolet";
-        c2.modelo = "Corsa";
-        c2.ano = 1988;
-        c2.km = 250000;
-        c2.cor = "cinza";
-        c2.preco = 8000;
+        c2.setMarca("Chevrolet");
+        c2.setModelo("Corsa");
+        c2.setAno(1988);
+        c2.setKm(250000);
+        c2.setCor("cinza");
+        c2.setPreco(8000);
 
-        c1.exibirAnuncio();
-        c1.simuladorSeguro(3);
+
+
 
         System.out.println("===========================");
         c2.exibirAnuncio();
@@ -43,5 +43,6 @@ public class UaiBiMotors {
         else {
             System.out.println("O valor do IPva: "+ ipvaC2);
         }
+        
     }
 }
